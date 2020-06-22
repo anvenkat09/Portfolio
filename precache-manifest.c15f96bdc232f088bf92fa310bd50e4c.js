338811f6a7d22bfa9ae72ec3ddbda3ac90b7fdfa
anvenkat09 <anirudh.venkatesh@gmail.com>
2020-06-22 01:54:20 -0700

    Updates

---
+++ precache-manifest.c15f96bdc232f088bf92fa310bd50e4c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5ed4cb92f15af160c0921e87c53dd6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9ae153ee37b775898a2f",
+    "url": "/portfolio/static/css/2.433215ff.chunk.css"
+  },
+  {
+    "revision": "eea0a70bcc2db7da1b57",
+    "url": "/portfolio/static/css/main.0fe1986f.chunk.css"
+  },
+  {
+    "revision": "9ae153ee37b775898a2f",
+    "url": "/portfolio/static/js/2.15de9257.chunk.js"
+  },
+  {
+    "revision": "358385828b55f8728566cc706231bf37",
+    "url": "/portfolio/static/js/2.15de9257.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eea0a70bcc2db7da1b57",
+    "url": "/portfolio/static/js/main.5c1ea97a.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "4f424d85b2d6c3723f86f498cfcc521e",
+    "url": "/portfolio/static/media/Anipic.4f424d85.png"
+  },
+  {
+    "revision": "09e3dd1fe5fe2741017261eed070dd74",
+    "url": "/portfolio/static/media/VenkateshAnirudh_SWE_Resume.09e3dd1f.pdf"
+  },
+  {
+    "revision": "24b6d96b0a7e16e3a26a667ae70488d6",
+    "url": "/portfolio/static/media/android_app_img.24b6d96b.png"
+  },
+  {
+    "revision": "609c3bf156e8a5657b29601862a2c527",
+    "url": "/portfolio/static/media/digit_class_app_img.609c3bf1.png"
+  },
+  {
+    "revision": "9be55c7eb789404a23a0572717ffed61",
+    "url": "/portfolio/static/media/facedetectbackendimg.9be55c7e.png"
+  },
+  {
+    "revision": "168f88d71287bd7c11729c4823e4e0c8",
+    "url": "/portfolio/static/media/facedetectimg.168f88d7.png"
+  },
+  {
+    "revision": "172110ac22fa3ac994aa0009ac6f141d",
+    "url": "/portfolio/static/media/kerasicon.172110ac.svg"
+  },
+  {
+    "revision": "a8b1c01d1c2844963be2aff6e1a12ffd",
+    "url": "/portfolio/static/media/portfolioimg.a8b1c01d.png"
+  },
+  {
+    "revision": "c213c3ee3212fe1c303c41c78ba6660d",
+    "url": "/portfolio/static/media/tficon.c213c3ee.svg"
+  },
+  {
+    "revision": "09fbc3b82196c5076c918b3e097966d4",
+    "url": "/portfolio/static/media/thesis_img.09fbc3b8.png"
+  }
+]);
